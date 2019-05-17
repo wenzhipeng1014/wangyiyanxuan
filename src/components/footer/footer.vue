fc97d018c225b389000118eee765d5d3cba463d5
wenzhipeng <1225381658@qq.com>
2019-05-17 13:24:40 +0800

    'one'

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer id="footer">
+    <div class="footer-nav">
+      <ul>
+        <li>首页</li>
+        <li>分类</li>
+        <li>识物</li>
+        <li>购物车</li>
+        <li>个人</li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: ""
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
+
+
